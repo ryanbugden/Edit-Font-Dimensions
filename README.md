c231c8582fa912a3ffa23fe58d29dbd15ed4a9a0
Ryan Bugden <rbugden@gmail.com>
2020-04-03 16:34:11 -0400

    Add lock-to-upm functionality
    Updated README and version number. There is now a checkbox that appears which gives you control as to whether ascender and descender’s relationship should remain locked.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Robofont extension for quickly manipulating your font info’s “Dimensions” 
 ## Instructions:
 * Install extension once.
 * Click toolbar icon.
+* Decide whether you want to lock the ascender / descender relationship to the UPM or not. Default is no. If you change the descender and then toggle the check box on, the ascender will snap to the UPM—and vice versa. (this feature idea thanks to Dyana Weissman)
 * Move font dimension values. (temporarily selectable as guidelines)
 * Exit tool and continue on with your life.
 
@@ -12,9 +13,16 @@ Robofont extension for quickly manipulating your font info’s “Dimensions” 
 
 ![](./_images/editFontDimensions_demo.gif)
 
+
+## To do:
+* Support locking not just asc-desc, but max y and min y, perhaps.
+* Figure out how to save user’s last preference...
+
+
 ### Versions
 
 ```
+1.2.0 2020.04.03  Supporting locking asc-desc relationship to UPM. Thanks for the suggestion, Dyana Weissman!
 1.1.1 2020-02-05  Fix need to unlock guides when using tool (if guides were locked). Thanks for the note, CJ Dunn!
 1.0.0 2020-01-24  Initial release
 ```
--- editFontDimensions.roboFontExt/lib/EditFontDimensionsTool.py
+++ editFontDimensions.roboFontExt/lib/EditFontDimensionsTool.py
@@ -1,6 +1,8 @@
 from mojo.events import EditingTool, installTool
-from mojo.UI import getDefault, setDefault
+from mojo.UI import getDefault, setDefault, CurrentGlyphWindow
+# from mojo.extensions import setExtensionDefault, getExtensionDefault, registerExtensionDefaults, removeExtensionDefault
 from AppKit import NSImage
+from vanilla import CheckBox
 from os import path
 
 dirname = path.dirname(__file__)
@@ -13,7 +15,9 @@ class EditFontDimensions(EditingTool):
     your font info’s Dimensions values.
     
     Ryan Bugden
-    2020.01.24
+    1.2.0 2020.04.03
+    1.1.1 2020.02.05
+    1.0.0 2020.01.24 
     '''
     
     def setup(self):
@@ -21,7 +25,22 @@ class EditFontDimensions(EditingTool):
         
     def becomeActive(self):
         self.f = CurrentFont()
+        self.w = CurrentGlyphWindow()
         
+        self.desc_driving = False
+        
+        # adding the UPM lock checkbox
+        self.lock2upm = False
+        self.checkbox = CheckBox(
+            (-200,40,200,30), 
+            "Lock Asc/Desc to UPM", 
+            callback=self.setUPMLock, 
+            value=self.lock2upm, 
+            sizeStyle='regular'
+            )
+        self.w.addGlyphEditorSubview(self.checkbox)
+        
+        # setting up guides and invisible names...
         self.verts = {
             "             "  : [self.f.info.descender, "descender"],
             "            "   : [self.f.info.xHeight,   "xHeight"],
@@ -32,8 +51,16 @@ class EditFontDimensions(EditingTool):
         for vert in self.verts.keys():
             self.f.appendGuideline(position=(0, self.verts[vert][0]), angle=0, name=vert, color=(0,0,0,1))
             
+        # saving whether user has guides locked previously
         self.user_lock = getDefault("glyphViewLockGuides")
         setDefault("glyphViewLockGuides", False)
+    
+    def setUPMLock(self, sender):
+        if sender.get() == 0:
+            self.lock2upm = False
+        else:
+            self.lock2upm = True
+            self.setMetrics()
 
     def becomeInactive(self):
         self.f = CurrentFont()
@@ -44,6 +71,14 @@ class EditFontDimensions(EditingTool):
                 
         setDefault("glyphViewLockGuides", self.user_lock)
         
+        self.w.removeGlyphEditorSubview(self.checkbox)
+        
+    def mouseDown(self, point, clickCount):
+        if abs(point.y - self.f.info.descender) < abs(self.f.info.ascender - point.y):
+            self.desc_driving = True
+        else:
+            self.desc_driving = False
+    
     def mouseDragged(self, point, delta):
         self.setMetrics()
                 
@@ -56,6 +91,13 @@ class EditFontDimensions(EditingTool):
         vert_settings = {}
         for guideline in self.f.guidelines:
             if guideline.name in self.verts.keys():
+                if self.desc_driving == True and self.lock2upm == True:
+                    if guideline.name == list(self.verts.keys())[3]:
+                        guideline.y = self.f.info.descender + self.f.info.unitsPerEm
+                elif self.desc_driving == False and self.lock2upm == True:
+                    if guideline.name == list(self.verts.keys())[0]:
+                        guideline.y = self.f.info.ascender - self.f.info.unitsPerEm
+                        
                 setattr(self.f.info, self.verts[guideline.name][1], guideline.y) 
             
     def getToolbarTip(self):
